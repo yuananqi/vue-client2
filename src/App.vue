2ddbd7611ce37e4e0b93d7f6d4f89920cb27d518
peiqi <peiqi@sina.com>
2019-07-08 11:53:07 +0800

    demo

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+
+<template>
+  <div id="app">
+   <router-view/>
+   <FooterGuide v-if="$route.meta.isShowFooter"/>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import {reqAddress} from './api'
+import FooterGuide from './components/FooterGuide/FooterGuide'
+  export default {
+    name:"App",
+    data(){
+      return{
+        msg:'App Component'
+      }
+    },
+    
+async mounted(){
+  this.$store.dispatch('getAddress')
+},
+
+
+
+    components:{
+      FooterGuide
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+ 
+</style>
+
